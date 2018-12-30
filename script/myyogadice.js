a76b600006c0d8a436b2a23c9f9c4f8e9566e90f
Tim Addison <git@tjaddison.com>
2018-12-29 23:29:20 -0500

    Add roll logic

--- script/myyogadice.js
+++ script/myyogadice.js
@@ -15,3 +15,22 @@ class MyYogaDice {
 
 var die = new MyYogaDice();
 
+const rollButton = document.getElementById('roll');
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/getElementsByClassName
+// Get every element with the class name of poseImage
+const poseElements = document.getElementsByClassName("poseImage");
+let poseImages = [];
+
+// Add each element to the array of poses
+for(let element of poseElements) {
+    poseImages.push(element);
+}
+
+rollButton.addEventListener('click', function() {
+    const poses = die.rollTheDice();
+    for(let i = 0; i < poses.length; i++) {
+        debugger;
+        poseImages[i].src = poses[i];
+    }
+});
