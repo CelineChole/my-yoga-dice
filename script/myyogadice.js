e8d7ccc84239194288fbdb2d4b28d69f8ce078da
Tim Addison <git@tjaddison.com>
2018-12-29 23:45:26 -0500

    add random poses

--- script/myyogadice.js
+++ script/myyogadice.js
@@ -1,3 +1,23 @@
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
 class YogaPose {
     constructor(name, image) {
         this.name = name;
@@ -8,18 +28,38 @@ class YogaPose {
 class MyYogaDice {
     constructor() {
         this.defaultImage = 'images/default.jpg';
+        this.poses = [];
+    }
+
+    addPose(name, imageUri = this.defaultImage) {
+        this.poses.push(new YogaPose(name,imageUri));
     }
 
     rollTheDice(numberOfPoses) {
         let posePictures = [];
+        // Shuffle available poses into a new array
+        let availablePoses = shuffle(this.poses.slice());
+
+        // Pop items from the array until we have as many as we need
         for(let i = 0; i < numberOfPoses; i++) {
-            posePictures.push(new YogaPose(`Pose ${i}`, this.defaultImage));
+            if(availablePoses.length > 0) {
+                posePictures.push(availablePoses.pop());
+            } else {
+                // If we run out of poses pop a default pose
+                posePictures.push(new YogaPose('All out of poses!', this.defaultImage));
+            }
         }
         return posePictures;
     }
 }
 
 var die = new MyYogaDice();
+die.addPose('Pigeon');
+die.addPose('Fish');
+die.addPose('Cobra');
+die.addPose('Crow');
+die.addPose('Standing Forward Bend');
+die.addPose('Triangle');
 
 const rollButton = document.getElementById('roll');
 
